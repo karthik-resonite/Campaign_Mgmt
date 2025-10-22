c7f776a8598fff00ab168422aea2bd5c71f2c0b1
Karthik B <v_karthik.b@hypersonix.ai>
2025-10-22 13:07:49 +0530

    changes

--- backend/app/twilio_calllog.py
+++ backend/app/twilio_calllog.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 # Twilio credentials
-account_sid = "ACcbc75d147cf1520dfa533d94a851856b"
-auth_token = "cc49339cc44325ed0150f41a398df924"
+# account_sid = "ACcbc75d147cf1520dfa533d94a851856b"
+# auth_token = "cc49339cc44325ed0150f41a398df924"
 client = Client(account_sid, auth_token)
 # Fetch all calls from a specific number
 calls = client.calls.list(from_="+17473024240", page_size=2000)
